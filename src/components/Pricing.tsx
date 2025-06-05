22947a9ce928c9f7dd4629d31607c3ef53a02117
Marcos Conde <marcosconde.amb@gmail.com>
2025-06-05 16:37:43 -0300

    ajuste no preço

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -139,7 +139,7 @@ export default function Pricing() {
                 {/* Pricing */}
                 <div className="mb-6">
                   <div className="flex items-baseline">
-                  <span className="text-secondary-400">{isAnnual && plan.name === 'GOLD' ? 'equivalente a  ' : ''}</span>
+                  <span className="text-secondary-400 text-sm">{isAnnual && plan.name === 'GOLD' ? 'equivalente a  ' : ''}</span>
                     <span className="text-4xl font-bold text-secondary-900 ml-3">
                       {plan.name === 'GOLD' 
                         ? (isAnnual ? plan.annualPrice : plan.monthlyPrice)
@@ -156,7 +156,7 @@ export default function Pricing() {
                         {isAnnual ? plan.annualOriginalPrice : plan.monthlyOriginalPrice}
                       </span>
                       <span className="text-accent-600 font-semibold text-sm">
-                        Economize {isAnnual ? '50%' : '28%'} na oferta de lançamento
+                        Economize {isAnnual ? '50%' : '28%'} no lançamento
                       </span>
                     </div>
                   )}
